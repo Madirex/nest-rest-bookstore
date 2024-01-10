e17f30ea59f62352fb9c229101efa8f97eea5ca4
Madirex Land <madirexland@gmail.com>
2024-01-10 19:41:36 +0100

    feat(configuration): project configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { BooksModule } from './books/books.module';
 
+/**
+ * Módulo principal de la aplicación
+ */
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'FUNKOS_DB',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true,
+    }),
+    BooksModule,
+  ],
   controllers: [],
   providers: [],
 })
+
+/**
+ * Módulo principal de la aplicación
+ */
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
+/*
+ * Función principal de la aplicación
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.setGlobalPrefix(process.env.API_VERSION || 'v1')
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(process.env.API_PORT || 3000)
 }
 bootstrap()
