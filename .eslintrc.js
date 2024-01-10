18aa6c224add538bc69992eaa21a7bb611e81a50
Madirex Land <madirexland@gmail.com>
2024-01-10 19:26:03 +0100

    feat(configuration): project configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+    'no-var': 'error',
+    'semi': 'off',
   },
 };
