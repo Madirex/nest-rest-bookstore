871b8b5ce33e909144698d431976f369bd962a79
Madirex Land <madirexland@gmail.com>
2024-01-11 16:39:51 +0100

    refactor(controller): controlador de Books refactorizado

--- src/books/controller/books.controller.spec.ts
+++ src/books/controller/books.controller.spec.ts
@@ -1,20 +1,200 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { NotFoundException } from '@nestjs/common'
 import { BooksController } from './books.controller'
-import { BooksService } from './service/books.service'
+import { BooksService } from '../service/books.service'
+import { CreateBookDto } from '../dto/create-book.dto'
+import { UpdateBookDto } from '../dto/update-book.dto'
+import { Request } from 'express'
+import { CacheModule } from '@nestjs/cache-manager'
+import { ResponseBookDto } from '../dto/response-book.dto'
 
 describe('BooksController', () => {
   let controller: BooksController
+  let service: BooksService
+
+  const booksServiceMock = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+    updateImage: jest.fn(),
+  }
+
+  const id = 2
+  const date = new Date()
+
+  const mockResult: ResponseBookDto = {
+    id: id,
+    name: 'Book1',
+    price: 100,
+    stock: 10,
+    author: 'test',
+    publisherId: 1,
+    description: 'test',
+    image: 'test',
+    category: 'test',
+    createdAt: date,
+    updatedAt: date,
+    isActive: true,
+  }
+
+  const createBookDto: CreateBookDto = {
+    name: 'Book Ejemplo',
+    author: 'Autor Ejemplo',
+    publisherId: 1,
+    category: 'test',
+    image: 'https://www.madirex.com/favicon.ico',
+    description: 'Descripción Ejemplo',
+    price: 19.99,
+    stock: 10,
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register()],
       controllers: [BooksController],
-      providers: [BooksService],
+      providers: [{ provide: BooksService, useValue: booksServiceMock }],
     }).compile()
 
     controller = module.get<BooksController>(BooksController)
+    service = module.get<BooksService>(BooksService)
   })
 
-  it('should be defined', () => {
+  it('debería estar definido', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('findAll', () => {
+    it('debería retornar todos los Books', async () => {
+      const testBooks: ResponseBookDto[] = [mockResult]
+
+      jest.spyOn(service, 'findAll').mockResolvedValue(testBooks)
+
+      expect(await controller.findAll()).toEqual(testBooks)
+      expect(service.findAll).toHaveBeenCalled()
+    })
+  })
+
+  describe('findOne', () => {
+    it('debería de recibir el Book dado el ID', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockResult)
+      await controller.findOne(id)
+
+      expect(service.findOne).toHaveBeenCalledWith(id)
+      expect(mockResult).toEqual({
+        id,
+        name: 'Book1',
+        price: 100,
+        stock: 10,
+        image: 'test',
+        category: 'test',
+        createdAt: date,
+        updatedAt: date,
+        isActive: true,
+      })
+    })
+
+    it('debería hacer un throw NotFoundException porque el Book no existe', async () => {
+      jest.spyOn(service, 'findOne').mockRejectedValue(new NotFoundException())
+      await expect(controller.findOne(id)).rejects.toThrow(NotFoundException)
+    })
+  })
+
+  describe('create', () => {
+    it('debería de crear un  Book', async () => {
+      jest.spyOn(service, 'create').mockResolvedValue(mockResult)
+      await controller.create(createBookDto)
+
+      expect(service.create).toHaveBeenCalledWith(createBookDto)
+      expect(mockResult).toEqual({
+        id: id,
+        name: 'Book1',
+        price: 100,
+        stock: 10,
+        image: 'test',
+        category: 'test',
+        createdAt: date,
+        updatedAt: date,
+        isActive: true,
+      })
+    })
+  })
+
+  describe('update', () => {
+    it('debería de actualizar el Book', async () => {
+      const dto: UpdateBookDto = { name: 'UpdatedBook' }
+
+      jest.spyOn(service, 'update').mockResolvedValue(mockResult)
+      await controller.update(id, dto)
+
+      expect(service.update).toHaveBeenCalledWith(id, dto)
+      expect(mockResult).toEqual({
+        id,
+        name: 'UpdatedBook',
+        price: 100,
+        stock: 10,
+        image: 'test',
+        category: 'test',
+        createdAt: date,
+        updatedAt: date,
+        isActive: true,
+      })
+    })
+
+    it('debería hacer throw NotFoundException porque el Book no existe', async () => {
+      const dto: UpdateBookDto = { name: 'UpdatedBook' }
+      jest.spyOn(service, 'update').mockRejectedValue(new NotFoundException())
+      await expect(controller.update(id, dto)).rejects.toThrow(
+        NotFoundException,
+      )
+    })
+  })
+
+  describe('remove', () => {
+    it('debería eliminar un Book', async () => {
+      jest.spyOn(service, 'remove').mockResolvedValue(mockResult)
+      await controller.remove(id)
+
+      expect(service.remove).toHaveBeenCalledWith(id)
+      expect(mockResult).toEqual({
+        id,
+        name: 'Book1',
+        price: 100,
+        stock: 10,
+        image: 'test',
+        category: 'test',
+        createdAt: date,
+        updatedAt: date,
+        isActive: true,
+      })
+    })
+
+    it('debería lanzarse un throw NotFoundException porque el Book no existe', async () => {
+      jest.spyOn(service, 'remove').mockRejectedValue(new NotFoundException())
+      await expect(controller.remove(id)).rejects.toThrow(NotFoundException)
+    })
+  })
+
+  describe('updateImage', () => {
+    it('debería actualizar la imagen del Book', async () => {
+      const mockFile = {
+        originalname: 'test.png',
+        mimetype: 'image/png',
+      } as Express.Multer.File
+      const mockReq = {} as Request
+      const mockResult: ResponseBookDto = new ResponseBookDto()
+
+      jest.spyOn(service, 'updateImage').mockResolvedValue(mockResult)
+
+      await controller.updateImage(id, mockFile, mockReq)
+      expect(service.updateImage).toHaveBeenCalledWith(
+        id,
+        mockFile,
+        mockReq,
+        true,
+      )
+      expect(mockResult).toBeInstanceOf(ResponseBookDto)
+    })
   })
 })
--- src/books/controller/books.controller.ts
+++ src/books/controller/books.controller.ts
@@ -1,42 +1,156 @@
 import {
-  Controller,
-  Get,
-  Post,
+  BadRequestException,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  HttpCode,
+  Logger,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Req,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common'
-import { BooksService } from './service/books.service'
-import { CreateBookDto } from './dto/create-book.dto'
-import { UpdateBookDto } from './dto/update-book.dto'
+import { BooksService } from '../service/books.service'
+import { CreateBookDto } from '../dto/create-book.dto'
+import { UpdateBookDto } from '../dto/update-book.dto'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { diskStorage } from 'multer'
+import { extname } from 'path'
+import { Request } from 'express'
+import { Util } from '../../util/util'
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager'
 
+/**
+ * Controlador de Books
+ */
 @Controller('books')
+@UseInterceptors(CacheInterceptor)
 export class BooksController {
+  private readonly logger = new Logger(BooksController.name)
+
+  /**
+   * Constructor
+   * @param booksService Servicio de Books
+   */
   constructor(private readonly booksService: BooksService) {}
 
-  @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.booksService.create(createBookDto)
-  }
-
+  /**
+   * Obtiene todos los Books
+   * @returns Arreglo con todos los Books
+   * @example http://localhost:3000/v1/books
+   */
   @Get()
-  findAll() {
-    return this.booksService.findAll()
+  @CacheKey('all_books')
+  @CacheTTL(30)
+  @HttpCode(200)
+  async findAll() {
+    this.logger.log('Obteniendo todos los Books')
+    return await this.booksService.findAll()
   }
 
+  /**
+   * Obtiene un Book dado el ID
+   * @param id Identificador del Book
+   * @returns Book encontrado
+   * @example http://localhost:3000/v1/books/1
+   */
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.booksService.findOne(+id)
+  @HttpCode(200)
+  async findOne(@Param('id') id: number) {
+    this.logger.log(`Obteniendo Book por id: ${id}`)
+    return await this.booksService.findOne(id)
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.update(+id, updateBookDto)
+  /**
+   * Crea un Book
+   * @param createBookDto DTO de creación de Book
+   * @returns Book creado
+   * @example http://localhost:3000/v1/books
+   */
+  @Post()
+  @HttpCode(201)
+  async create(@Body() createBookDto: CreateBookDto) {
+    this.logger.log(`Creando Book con datos: ${JSON.stringify(createBookDto)}`)
+    return await this.booksService.create(createBookDto)
   }
 
+  /**
+   * Actualiza un Book dado el ID
+   * @param id Identificador del Book
+   * @param updateBookDto DTO de actualización de Book
+   * @returns Book actualizado
+   * @example http://localhost:3000/v1/books/1
+   */
+  @Put(':id')
+  @HttpCode(200)
+  async update(@Param('id') id: number, @Body() updateBookDto: UpdateBookDto) {
+    this.logger.log(
+      `Actualizando Book ${id} con datos: ${JSON.stringify(updateBookDto)}`,
+    )
+    return await this.booksService.update(id, updateBookDto)
+  }
+
+  /**
+   * Elimina un Book dado el ID
+   * @param id Identificador del Book
+   * @returns Book eliminado
+   * @example http://localhost:3000/v1/books/1
+   */
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.booksService.remove(+id)
+  @HttpCode(204)
+  async remove(@Param('id') id: number) {
+    this.logger.log(`Eliminando Book con id: ${id}`)
+    return await this.booksService.remove(id)
+  }
+
+  /**
+   * Actualiza la imagen de un Book
+   * @param id Identificador del Book
+   * @param file Fichero de imagen
+   * @param req Request
+   */
+  @Patch('/image/:id')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: process.env.UPLOADS_DIR || './storage-dir',
+        filename: (req, file, cb) => {
+          const dateTime = Util.getCurrentDateTimeString()
+          const uuid = req.params.id
+          const fileName = `${uuid}-${dateTime}`
+          const fileExt = extname(file.originalname)
+          cb(null, `${fileName}${fileExt}`)
+        },
+      }),
+    }),
+  )
+  async updateImage(
+    @Param('id') id: number,
+    @UploadedFile() file: Express.Multer.File,
+    @Req() req: Request,
+  ) {
+    this.logger.log(`Actualizando imagen al Book con id ${id}:  ${file}`)
+
+    const allowedMimes = ['image/jpeg', 'image/png']
+    const maxFileSizeInBytes = 1024 * 1024 // 1 megabyte
+    if (file === undefined) throw new BadRequestException('Fichero no enviado')
+    else if (!allowedMimes.includes(file.mimetype)) {
+      throw new BadRequestException(
+        'Fichero no soportado. No es del tipo imagen válido',
+      )
+    } else if (file.mimetype != Util.detectFileType(file)) {
+      throw new BadRequestException(
+        'Fichero no soportado. No es del tipo imagen válido',
+      )
+    } else if (file.size > maxFileSizeInBytes) {
+      throw new BadRequestException(
+        `El tamaño del archivo no puede ser mayor a ${maxFileSizeInBytes} bytes.`,
+      )
+    }
+    return await this.booksService.updateImage(id, file, req, true)
   }
 }
