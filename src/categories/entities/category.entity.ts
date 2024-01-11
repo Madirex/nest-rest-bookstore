723b8ce64094125fa222379e16dab6f33df7a078
Madirex Land <madirexland@gmail.com>
2024-01-11 16:41:00 +0100

    feat(entity): agregada entidad de categoría

---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,61 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { Book } from '../../books/entities/book.entity'
+
+/**
+ * @description Enumeración de los tipos de categorías
+ */
+export enum CategoryType {
+  SERIES = 'SERIES',
+  DISNEY = 'DISNEY',
+  SUPERHEROS = 'SUPERHEROS',
+  MOVIE = 'MOVIE',
+  OTHER = 'OTHER',
+}
+
+/**
+ * @description Entidad de la categoría
+ */
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    name: 'category_type',
+    type: 'enum',
+    enum: CategoryType,
+    default: CategoryType.OTHER,
+  })
+  categoryType: CategoryType
+
+  @Column({ name: 'name', type: 'varchar', length: 255 })
+  name: string
+
+  @OneToMany(() => Book, (book) => book.category)
+  books: Book[]
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date
+
+  @Column({ name: 'is_active', type: 'boolean', default: true })
+  isActive: boolean
+}
