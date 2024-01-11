7b450a3aa3e3b1edea1e4175169fa82019d11644
Madirex Land <madirexland@gmail.com>
2024-01-11 13:26:51 +0100

    config(module): agregada configuración de Books

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
-import { BooksService } from './books.service';
-import { BooksController } from './books.controller';
+import { Module } from '@nestjs/common'
+import { BooksService } from './books.service'
+import { BooksController } from './books.controller'
+import { BookMapper } from './mappers/book.mapper'
+import { CacheModule } from '@nestjs/cache-manager'
+import { StorageModule } from '../storage/storage.module'
+import { Category } from '../categories/entities/category.entity'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Book } from './entities/book.entity'
+import { NotificationsModule } from '../websockets/notifications/notifications.module'
 
+/**
+ * Módulo de Books
+ */
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Book]),
+    TypeOrmModule.forFeature([Category]),
+    StorageModule,
+    NotificationsModule,
+    CacheModule.register(),
+  ],
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [BooksService, BookMapper],
 })
 export class BooksModule {}
