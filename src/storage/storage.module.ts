dcf72349b8a1434d2e6d1c34b6bf5a86ff646d51
Madirex Land <madirexland@gmail.com>
2024-01-11 16:43:09 +0100

    feat(module): agregado módulo de storage

---
+++ src/storage/storage.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { StorageService } from './storage.service';
+import { StorageController } from './storage.controller';
+
+@Module({
+  controllers: [StorageController],
+  providers: [StorageService],
+  exports: [StorageService], // exportamos el servicio
+})
+export class StorageModule {}
