211fc139a48039566e78dd6f573eda7dc531a0c3
Daniel Garrido Muros <daniell.gm03@gmail.com>
2024-01-23 11:00:26 +0100

    style(storage): Pasamos prettier a storage

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { StorageService } from './storage.service';
-import { StorageController } from './storage.controller';
+import { Module } from '@nestjs/common'
+import { StorageService } from './storage.service'
+import { StorageController } from './storage.controller'
 
 @Module({
   controllers: [StorageController],
