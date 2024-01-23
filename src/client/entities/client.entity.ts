c485cb3ba2a7560087e6a9c7c51e9c5ea86decfa
Daniel Garrido Muros <daniell.gm03@gmail.com>
2024-01-23 11:03:10 +0100

    feat(entity): Creamos entidad client

---
+++ src/client/entities/client.entity.ts
@@ -0,0 +1,37 @@
+import { Address } from 'src/common/address.entity'
+import { Column, Entity, PrimaryColumn } from 'typeorm'
+
+@Entity('clients')
+export class Client {
+  static readonly IMAGE_DEFAULT = 'https://via.placeholder.com/150'
+
+  @PrimaryColumn({ type: 'uuid' })
+  id: string
+
+  @Column('varchar', { length: 255, nullable: false })
+  name: string
+
+  @Column('varchar', { length: 255, nullable: false })
+  surname: string
+
+  @Column('varchar', { length: 255, nullable: false })
+  email: string
+
+  @Column('varchar', { length: 255, nullable: false })
+  phone: string
+
+  @Column((type) => Address)
+  address: Address
+
+  @Column('varchar', { length: 255, nullable: false, default: Client.IMAGE_DEFAULT })
+  image: string
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date
+
+  @Column('timestamp', {
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date
+}
