e77f30a9830779b11d9f8762be06f1ec54f14007
Alexandru <alexandru.dorobat@educa.madrid.org>
2024-01-26 17:08:17 +0100

    fix(module): adaptado module de shop

--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ShopService } from './shop.service';
-import { ShopController } from './controller/shop.controller';
+import { ShopsService } from './services/shop.service';
+import { ShopsController } from './controller/shop.controller';
+import { ShopMapper } from './mappers/shop.mapper';
+import { CacheModule } from '@nestjs/cache-manager';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shop } from './entities/shop.entity';
+import { Client } from '../client/entities/client.entity';
+import { Book } from '../books/entities/book.entity';
+import { NotificationsModule } from '../websockets/notifications/notifications.module';
 
+/**
+ * Módulo de Shops
+ */
 @Module({
-  controllers: [ShopController],
-  providers: [ShopService],
+  imports: [
+    TypeOrmModule.forFeature([Shop, Client, Book]),
+    NotificationsModule,
+    CacheModule.register(),
+  ],
+  controllers: [ShopsController],
+  providers: [ShopsService, ShopMapper],
 })
-export class ShopModule {}
+export class ShopsModule {}
