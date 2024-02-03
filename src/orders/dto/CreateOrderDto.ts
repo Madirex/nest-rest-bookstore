28f91983874f57ea6cac7f5f0e0ae3d99066f579
Madirex Land <madirexland@gmail.com>
2024-02-03 21:51:44 +0100

    refactor(orders): refactorización del DTO de pedidos

--- src/orders/dto/CreateOrderDto.ts
+++ src/orders/dto/CreateOrderDto.ts
@@ -1,14 +1,17 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 import { OrderLineDto } from './OrderLineDto'
 
+/**
+ * The CreateOrderDto class is a data transfer object that is used to define the structure of the data that is used to create an order.
+ */
 export class CreateOrderDto {
   @IsNumber()
   @IsNotEmpty()
-  idUser: number
+  userId: string
 
   @IsString()
   @IsNotEmpty()
-  idClient: string
+  clientId: string
 
   @IsNotEmpty()
   orderLines: OrderLineDto[]
