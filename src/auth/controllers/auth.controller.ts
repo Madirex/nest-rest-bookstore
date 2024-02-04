f1f325d214cbb552f6d66922a2cb663b375b274e
Madirex Land <madirexland@gmail.com>
2024-02-04 14:16:07 +0100

    doc(auth): agregada documentación Swagger a Auth

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,11 +2,18 @@ import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from '../services/auth.service'
 import { UserSignUpDto } from '../dto/user-sign.up.dto'
 import { UserSignInDto } from '../dto/user-sign.in.dto'
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
 
 /**
  * @description Controlador de autenticación
  */
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   /**
    * @description Constructor del controlador
@@ -19,6 +26,18 @@ export class AuthController {
    * @param userSignUpDto DTO de registro de usuario
    */
   @Post('signup')
+  @ApiResponse({
+    status: 201,
+    description: 'Cuenta creada',
+  })
+  @ApiBody({
+    description: 'Datos de la cuenta a crear',
+    type: UserSignUpDto,
+  })
+  @ApiBadRequestResponse({
+    description:
+      'En algunos de los campos no es válido según la especificación del DTO',
+  })
   async singUp(@Body() userSignUpDto: UserSignUpDto) {
     return await this.authService.singUp(userSignUpDto)
   }
@@ -28,6 +47,18 @@ export class AuthController {
    * @param userSignInDto DTO de login de usuario
    */
   @Post('signin')
+  @ApiResponse({
+    status: 201,
+    description: 'Inicio de sesión realizado',
+  })
+  @ApiBody({
+    description: 'Datos de la cuenta a iniciar sesión',
+    type: UserSignInDto,
+  })
+  @ApiBadRequestResponse({
+    description:
+      'Nombre o contraseña no válidos, según la especificación del DTO',
+  })
   async singIn(@Body() userSignInDto: UserSignInDto) {
     return await this.authService.singIn(userSignInDto)
   }
