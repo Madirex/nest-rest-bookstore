be96b7979406a364825d6c0e227191263a384416
Madirex Land <madirexland@gmail.com>
2024-02-04 21:03:28 +0100

    doc(categories): agregada documentación Swagger

--- src/categories/controller/categories.controller.ts
+++ src/categories/controller/categories.controller.ts
@@ -15,9 +15,19 @@ import { CategoriesService } from '../service/categories.service'
 import { CreateCategoryDto } from '../dto/create-category.dto'
 import { UpdateCategoryDto } from '../dto/update-category.dto'
 import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager'
-import { Paginate, PaginateQuery } from 'nestjs-paginate'
+import { Paginate, Paginated, PaginateQuery } from 'nestjs-paginate'
 import { Roles, RolesAuthGuard } from '../../auth/guards/roles-auth.guard'
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard'
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
+import { ResponseCategoryDto } from '../dto/response-category.dto'
 
 /**
  * Controlador de categorías
@@ -25,6 +35,7 @@ import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard'
 @Controller('categories')
 @UseInterceptors(CacheInterceptor)
 @UseGuards(JwtAuthGuard, RolesAuthGuard)
+@ApiTags('Categories')
 export class CategoriesController {
   private readonly logger = new Logger(CategoriesController.name)
 
@@ -45,6 +56,41 @@ export class CategoriesController {
   @CacheTTL(30)
   @HttpCode(200)
   @Roles('USER')
+  @ApiResponse({
+    status: 200,
+    description: 'Categorías encontradas',
+    type: Paginated<ResponseCategoryDto>,
+  })
+  @ApiQuery({
+    description: 'Filtro por límite por página',
+    name: 'limit',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    description: 'Filtro por página',
+    name: 'page',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    description: 'Filtro de ordenación: campo:ASC|DESC',
+    name: 'sortBy',
+    required: false,
+    type: String,
+  })
+  @ApiQuery({
+    description: 'Filtro de búsqueda: filter.campo = $eq:valor',
+    name: 'filter',
+    required: false,
+    type: String,
+  })
+  @ApiQuery({
+    description: 'Filtro de búsqueda: search = valor',
+    name: 'search',
+    required: false,
+    type: String,
+  })
   async findAll(@Paginate() query: PaginateQuery) {
     this.logger.log('Obteniendo todas las categorías')
     return await this.categoriesService.findAll(query)
@@ -59,6 +105,22 @@ export class CategoriesController {
   @Get(':id')
   @HttpCode(200)
   @Roles('USER')
+  @ApiResponse({
+    status: 200,
+    description: 'Categoría encontrada',
+    type: ResponseCategoryDto,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identificador de la categoría',
+    type: Number,
+  })
+  @ApiNotFoundResponse({
+    description: 'Categoría no encontrada',
+  })
+  @ApiBadRequestResponse({
+    description: 'El id de la categoría no es válido',
+  })
   async findOne(@Param('id') id: number) {
     this.logger.log(`Obteniendo categoría por id: ${id}`)
     return await this.categoriesService.findOne(+id)
@@ -73,6 +135,19 @@ export class CategoriesController {
   @Post()
   @HttpCode(201)
   @Roles('ADMIN')
+  @ApiResponse({
+    status: 201,
+    description: 'Categoría creada',
+    type: ResponseCategoryDto,
+  })
+  @ApiBody({
+    description: 'Datos de la categoría a crear',
+    type: CreateCategoryDto,
+  })
+  @ApiBadRequestResponse({
+    description:
+      'En algunos de los campos no es válido según la especificación del DTO',
+  })
   async create(@Body() createCategoryDto: CreateCategoryDto) {
     this.logger.log(
       `Creando categoría con datos: ${JSON.stringify(createCategoryDto)}`,
@@ -90,6 +165,26 @@ export class CategoriesController {
   @Put(':id')
   @HttpCode(200)
   @Roles('ADMIN')
+  @ApiResponse({
+    status: 200,
+    description: 'Categoría actualizada',
+    type: ResponseCategoryDto,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identificador de la categoría',
+    type: Number,
+  })
+  @ApiBody({
+    description: 'Datos de la categoría a actualizar',
+    type: UpdateCategoryDto,
+  })
+  @ApiBadRequestResponse({
+    description: 'El id de la categoría no es válido',
+  })
+  @ApiNotFoundResponse({
+    description: 'Categoría no encontrada',
+  })
   async update(
     @Param('id') id: number,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -111,6 +206,21 @@ export class CategoriesController {
   @Delete(':id')
   @HttpCode(204)
   @Roles('ADMIN')
+  @ApiResponse({
+    status: 204,
+    description: 'Categoría eliminada',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identificador de la categoría',
+    type: Number,
+  })
+  @ApiNotFoundResponse({
+    description: 'Categoría no encontrada',
+  })
+  @ApiBadRequestResponse({
+    description: 'El id de la categoría no es válido',
+  })
   async remove(@Param('id') id: number) {
     this.logger.log(`Eliminando categoría con id: ${id}`)
     return await this.categoriesService.remove(+id)
