346aa41efd7e28acc2300f3ded1311ac2ae589d6
Madirex Land <madirexland@gmail.com>
2024-02-08 00:27:28 +0100

    fix(websockets): arreglados errores en Shop Notifications

--- src/websockets/notifications/shop-notification.gateway.ts
+++ src/websockets/notifications/shop-notification.gateway.ts
@@ -27,7 +27,7 @@ export class ShopsNotificationsGateway {
   }
 
   /**
-   * @description Envía una notificación a los clientes conectados
+   * @description Envía una notificación a los tiendas conectados
    * @param notification Notificación a enviar
    */
   sendMessage(notification: WsNotification<ResponseShopDto>) {
@@ -35,12 +35,12 @@ export class ShopsNotificationsGateway {
   }
 
   /**
-   * @description Maneja la conexión de un cliente
-   * @param client Cliente conectado
+   * @description Maneja la conexión de un tienda
+   * @param shop tienda conectado
    * @private Método privado
    */
-  private handleConnection(client: Socket) {
-    this.logger.debug('Cliente conectado:', client.id)
+  private handleConnection(shop: Socket) {
+    this.logger.debug('tienda conectado:', shop.id)
     this.server.emit(
       'connection',
       'Shops Notifications WS: Conexión establecida',
@@ -48,11 +48,11 @@ export class ShopsNotificationsGateway {
   }
 
   /**
-   * @description Maneja la desconexión de un cliente
-   * @param client Cliente desconectado
+   * @description Maneja la desconexión de un tienda
+   * @param shop tienda desconectado
    * @private Método privado
    */
-  private handleDisconnect(client: Socket) {
-    this.logger.debug('Cliente desconectado:', client.id)
+  private handleDisconnect(shop: Socket) {
+    this.logger.debug('shop desconectado:', shop.id)
   }
 }
