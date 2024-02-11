3621a302f7f4a04173164ebe126d66698f143243
daniell.gm03 <daniell.gm03@gmail.com>
2024-02-11 02:46:54 +0100

    fix(dto): Arreglamos dto de orders

--- src/orders/dto/CreateOrderDto.ts
+++ src/orders/dto/CreateOrderDto.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
+import {IsArray, IsNotEmpty, IsNumber, IsString, IsUUID, MinLength, ValidateNested} from 'class-validator'
 import { OrderLineDto } from './OrderLineDto'
 import { ApiProperty } from '@nestjs/swagger'
+import {Type} from "class-transformer";
 
 /**
  * The CreateOrderDto class is a data transfer object that is used to define the structure of the data that is used to create an order.
@@ -9,21 +10,28 @@ export class CreateOrderDto {
   @ApiProperty({
     description: 'El identificador del usuario',
   })
-  @IsNumber()
-  @IsNotEmpty()
+  @IsString({ message: 'El id del usuario debe ser un string'})
+  @IsUUID('4', { message: 'El id del usuario debe ser un UUID'})
+  @IsNotEmpty({ message: 'El id del usuario no puede estar vacío'})
+  @MinLength(16, { message: 'El id del usuario debe tener una longitud de al menos 16 caracteres'})
   userId: string
 
   @ApiProperty({
     description: 'El identificador del cliente',
   })
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'El id del cliente debe ser un string'})
+  @IsUUID('4', { message: 'El id del usuario debe ser un UUID'})
+  @IsNotEmpty({ message: 'El id del cliente no puede estar vacío'})
+  @MinLength(16, { message: 'El id del cliente debe tener una longitud de al menos 16 caracteres'})
   clientId: string
 
   @ApiProperty({
     description: 'Las líneas de pedido',
     type: [OrderLineDto],
   })
-  @IsNotEmpty()
+  @IsArray({ message: 'Las líneas de pedido deben ser un array'})
+  @ValidateNested({ each: true })
+  @Type(() => OrderLineDto)
+  @IsNotEmpty({ message: 'Las líneas de pedido no pueden estar vacías'})
   orderLines: OrderLineDto[]
 }
--- src/orders/dto/UpdateOrderDto.ts
+++ src/orders/dto/UpdateOrderDto.ts
@@ -1,31 +1,39 @@
 import { CreateOrderDto } from './CreateOrderDto'
 import { PartialType } from '@nestjs/mapped-types'
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
+import {IsArray, IsNotEmpty, IsNumber, IsString, IsUUID, MinLength, ValidateNested} from 'class-validator'
 import { OrderLineDto } from './OrderLineDto'
 import { ApiProperty } from '@nestjs/swagger'
+import {Type} from "class-transformer";
 
 /**
  * The UpdateOrderDto class is a data transfer object that is used to define the structure of the data that is used to update an order.
  */
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
-  @IsNumber()
-  @IsNotEmpty()
   @ApiProperty({
     description: 'El identificador del usuario',
   })
+  @IsString({ message: 'El id del usuario debe ser un string'})
+  @IsUUID('4', { message: 'El id del usuario debe ser un UUID'})
+  @IsNotEmpty({ message: 'El id del usuario no puede estar vacío'})
+  @MinLength(16, { message: 'El id del usuario debe tener una longitud de al menos 16 caracteres'})
   userId: string
 
   @ApiProperty({
     description: 'El identificador del cliente',
   })
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'El id del cliente debe ser un string'})
+  @IsUUID('4', { message: 'El id del usuario debe ser un UUID'})
+  @IsNotEmpty({ message: 'El id del cliente no puede estar vacío'})
+  @MinLength(16, { message: 'El id del cliente debe tener una longitud de al menos 16 caracteres'})
   clientId: string
 
   @ApiProperty({
     description: 'Las líneas de pedido',
     type: [OrderLineDto],
   })
-  @IsNotEmpty()
+  @IsArray({ message: 'Las líneas de pedido deben ser un array'})
+  @ValidateNested({ each: true })
+  @Type(() => OrderLineDto)
+  @IsNotEmpty({ message: 'Las líneas de pedido no pueden estar vacías'})
   orderLines: OrderLineDto[]
 }
